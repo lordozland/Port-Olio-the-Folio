8924baa238919d73bcf6dcc2bdc53c681122bd3b
William Crews <crewswh@gmail.com>
2021-07-17 13:27:50 -0400

    says null

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const path = require('path');
 
-// This is the 'get' route 
 router.get('/', async (req, res) => {
-  // Here, index.html is rendered
+  res.sendFile(path.join(__dirname, '../views/main/main.handlbars'));
+});
+
+router.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
