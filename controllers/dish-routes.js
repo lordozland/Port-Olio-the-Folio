ff05c4e4a0c693162a40e19fdde35be8429de9f3
William Crews <crewswh@gmail.com>
2021-07-17 13:47:26 -0400

    starting over again

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -1,27 +1,16 @@
+// Is this a Model, a View, or a Controller? 
+// This file is a Controller. 
+// What is it responsible for handling?
+// It routes commands to the Model and View parts.
+
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//   res.render('main');
-// });
-
+// Add a comment describing the purpose of the 'get' route
+// GET route for getting all of the dishes that are on the menu
 router.get('/', async (req, res) => {
+  // Add a comment describing the purpose of the render method
+  // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
   res.render('all');
-});
-
-// router.get('/', (req, res) => {
-//   if (req.isAuthenticated()) {
-//     const user = {
-//       user: req.session.passport.user,
-//       isloggedin: req.isAuthenticated()
-//     };
-//     res.render('dashboard', user);
-//   } else {
-//     res.render('dashboard');
-//   }
-// });
-
-router.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
