ad376b18830a4b097dea5ccba5acfc27c72e3d68
William Crews <crewswh@gmail.com>
2021-07-17 13:45:17 -0400

    handlebars installation

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
-const path = require('path');
+
+// router.get('/', (req, res) => {
+//   res.render('main');
+// });
 
 router.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/main/main.handlebars'));
+  res.render('all');
 });
+
+// router.get('/', (req, res) => {
+//   if (req.isAuthenticated()) {
+//     const user = {
+//       user: req.session.passport.user,
+//       isloggedin: req.isAuthenticated()
+//     };
+//     res.render('dashboard', user);
+//   } else {
+//     res.render('dashboard');
+//   }
+// });
 
 router.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // Dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const path = require('path');
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 // Sets up the routes
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/dish-routes'));
 
 // Starts the server to begin listening
